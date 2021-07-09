8076a07233c3486e4505d11d9d55d662895bba8f
MRadzynski <radzynski.milosz@gmail.com>
2021-07-09 18:45:35 +0200

    Adding custom height unit to unified views on all devices

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import FoodForm from './pages/FoodForm/FoodForm';
 
+let root = document.documentElement;
+
+window.addEventListener('load', () => {
+  let vh = window.innerHeight / 100;
+  root.style.setProperty('--vh', vh);
+});
+
+window.addEventListener('orientationchange', () => {
+  window.location.reload();
+});
+
 function App() {
   return (
     <>
